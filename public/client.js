6b2bb9db8d2d148b7c9e799ea17769ef695e2ace
oleg-dorozhko <oleg.dorozhko@gmail.com>
2016-08-08 23:58:49 +0300

    Demo

--- public/client.js
+++ public/client.js
@@ -1,7 +1,48 @@
+function test_call_server()
+{
+	
+	
+	var canvas = document.getElementById("canvas");
+
+	canvas.toBlob( function(blob) { 
+	
+		//console.log(blob);
+			  
+		var xhr = new XMLHttpRequest();
+		xhr.open('POST', '/test_call', true);
+		
+		xhr.onload = function(e) {  
+		
+			if (xhr.readyState != 4) return;
+
+			if (xhr.status != 200) {    alert(xhr.status + ': ' + xhr.statusText); return;  }
+			
+			console.log("SERVEr ANSWER: "+xhr.responseText);
+			
+		}
+		
+
+		xhr.send(blob);
+		
+	});
+
+
+}
+
 window.onload = function()
 {
 	document.getElementById("canvas").onclick = function()
 	{
-		alert('test ok');
+		var ctx = this.getContext("2d");
+		ctx.fillStyle = "blue";
+		ctx.fillRect(100,100,200,200);
+	
+	}
+	
+	document.getElementById("test_call_server").onclick = function()
+	{
+		
+		test_call_server();
+		
 	}
 }

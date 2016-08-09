871ca97f9b1881a5625a04723ebf48d5467164d1
oleg-dorozhko <oleg.dorozhko@gmail.com>
2016-08-09 16:17:10 +0300

    Demo

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 var http = require('http'); 
 var PNG = require('pngjs').PNG;
 var fs = require('fs');
+var PATH = require('path'):
 
 
-
+console.log("\n------------------------------");
+console.log("\n-------------" + __dirname + "-----------------");
+console.log("\n------------------------------");
+/***
 const file = fs.createWriteStream('exampleexampleexample.txt');
 file.write('hello, ');
 file.end('world!');
+*****/
 

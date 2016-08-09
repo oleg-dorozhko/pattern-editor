e653883a8ba8ec26474b158b66c198af76042f54
oleg-dorozhko <oleg.dorozhko@gmail.com>
2016-08-09 16:27:39 +0300

    Demo

--- index.js
+++ index.js
@@ -8,6 +8,17 @@ console.log("\n------------------------------");
 console.log("\n-------------" + __dirname + "-----------------");
 console.log("\n------------------------------");
 
+/*****
 const file = fs.createWriteStream('/exampleexampleexample.txt');
 file.write('hello, ');
 file.end('world!');
+******/
+
+var fs = require('fs');
+fs.writeFile("exampleexampleexample.txt", "Hey there!", function(err) {
+    if(err) {
+        return console.log(err);
+    }
+
+    console.log("The file was saved!");
+}); 

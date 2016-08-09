332506fb8fc6458c1878bd371ef77dfdc9eb005e
oleg-dorozhko <oleg.dorozhko@gmail.com>
2016-08-09 16:39:56 +0300

    Demo

--- index.js
+++ index.js
@@ -7,7 +7,18 @@ var PATH = require('path');
 console.log("\n------------------------------");
 console.log("\n-------------" + __dirname + "-----------------");
 var fs = require('fs');
-var contents = fs.readFileSync('app.json').toString();
+var path = "./testfs.txt";
+var data = "Hello from the Node writeFile method!";
+
+fs.writeFile(path, data, function(error) {
+     if (error) {
+       console.error("write error:  " + error.message);
+     } else {
+       console.log("Successful Write to " + path);
+     }
+});
+
+var contents = fs.readFileSync('./testfs.txt').toString();
 console.log(contents); //esperamos por el resultado
 console.log("Hello!"); 
 console.log("\n------------------------------");

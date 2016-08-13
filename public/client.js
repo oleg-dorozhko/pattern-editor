b75edf017c4161ff45c3df4da2ceaefe7f2a8030
oleg-dorozhko <oleg.dorozhko@gmail.com>
2016-08-13 19:00:48 +0300

    brainlock drag and drop

--- public/client.js
+++ public/client.js
@@ -62,6 +62,129 @@ window.onload = function()
 		$("#fill").click( function() { send_seed(); } );
 		
 		// $("#canvas").click( function(ev) { whenClickedOnCanvas(ev); } );
+		
+		
+		
+		//drag and drop implementation
+		
+		document.onmousedown = function(e) {
+
+		  if (e.which != 1) { // если клик правой кнопкой мыши
+			return; // то он не запускает перенос
+		  }
+
+		  var elem = e.target.closest('.draggable');
+
+		  if (!elem) return; // не нашли, клик вне draggable-объекта
+
+		  // запомнить переносимый объект
+		  dragObject.elem = elem;
+
+		  // запомнить координаты, с которых начат перенос объекта
+		  dragObject.downX = e.pageX;
+		  dragObject.downY = e.pageY;
+		  
+		}
+		
+		
+		
+		
+		
+		document.onmousemove = function(e) {
+			
+		  if (!dragObject.elem) return; // элемент не зажат
+
+		  if ( !dragObject.avatar ) { // если перенос не начат...
+
+			// посчитать дистанцию, на которую переместился курсор мыши
+			var moveX = e.pageX - dragObject.downX;
+			var moveY = e.pageY - dragObject.downY;
+			if ( Math.abs(moveX) < 3 && Math.abs(moveY) < 3 ) {
+			  return; // ничего не делать, мышь не передвинулась достаточно далеко
+			}
+
+			dragObject.avatar = createAvatar(e); // захватить элемент
+			if (!dragObject.avatar) {
+			  dragObject = {}; // аватар создать не удалось, отмена переноса
+			  return; // возможно, нельзя захватить за эту часть элемента
+			}
+
+			// аватар создан успешно
+			// создать вспомогательные свойства shiftX/shiftY
+			var coords = getCoords(dragObject.avatar);
+			dragObject.shiftX = dragObject.downX - coords.left;
+			dragObject.shiftY = dragObject.downY - coords.top;
+
+			startDrag(e); // отобразить начало переноса
+		  }
+
+		  // отобразить перенос объекта при каждом движении мыши
+		  dragObject.avatar.style.left = e.pageX - dragObject.shiftX + 'px';
+		  dragObject.avatar.style.top = e.pageY - dragObject.shiftY + 'px';
+
+		  return false;
+		  
+		}
+		
+		
+		
+		
+		document.onmouseup = function(e) {
+		  // (1) обработать перенос, если он идет
+		  if (dragObject.avatar) {
+			finishDrag(e);
+		  }
+
+		  // в конце mouseup перенос либо завершился, либо даже не начинался
+		  // (2) в любом случае очистим "состояние переноса" dragObject
+		  dragObject = {};
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 				
 		document.onclick = function (ev) { 
 			if (!ev.target.hasAttribute('seed-clicked')) return;

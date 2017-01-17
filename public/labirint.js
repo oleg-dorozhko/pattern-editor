1417f00b23b09c5af46eb63f2501b871847b4afe
oleg-dorozhko <oleg.dorozhko@gmail.com>
2017-01-17 04:37:45 +0200

    Renovation

--- public/labirint.js
+++ public/labirint.js
@@ -2,6 +2,7 @@ var global_seed_size = 21;
 var global_selected_seed = null;
 var global_arr_objects = null;
 var glob_colors = null;
+var global_first_seed_image_data = null;
 
 function copyPasteFinished()
 {
@@ -234,6 +235,27 @@ function put_one_seed_into_right_canvas()
 	}
 }
 
+function free_image_equal(data1, data2)
+{
+	if(data1 == null) return;
+	if(data2 == null) return;
+	if(data2.length != data2.length) continue;
+	
+	var obj_data = data1; //arrObjects[i].data;
+	
+	var result = true;
+	for(var j=0;j<data2.length;j++)
+	{
+		if(obj_data[j] != data2[j])  
+		{
+			result=false;
+			break;
+		}
+	}
+	if(result==true) return i;
+	
+}
+
 
 function findImageDataInArrObjects( arrObjects, data )
 {
@@ -400,22 +422,160 @@ function whenUserRightClickOnLeftCanvas(e)
 	e.preventDefault();
 }
 
+function highlight()
+{
+	if(global_selected_seed != null)	
+	{
+			//alert('test ok');
+	//global_selected_seed = get_selected_seed(e);
+	
+	
+	var canvas = document.getElementById("left_canvas");
+	var context = canvas.getContext("2d");
+	var n = global_selected_seed[2];
+	var m = global_selected_seed[3];
+	var imgData = context.getImageData(n*global_seed_size,m*global_seed_size,global_seed_size,global_seed_size);
+	global_first_seed_image_data = cloneUnrealImageData(n,m,imgData);
+	
+	context.fillStyle = "rgba(255,0,255,0.5)";
+	context.fillRect(n*global_seed_size,m*global_seed_size,global_seed_size,global_seed_size);
+	/******
+	
+	var canvas2 = document.createElement("canvas");
+	canvas2.width = global_seed_size;
+	canvas2.height = global_seed_size;
+	canvas2.getContext("2d").putImageData(imgData, 0,0);
+	canvas2.onclick = function()
+	{
+		//alert(this.getAttribute("data-color"));
+		var points = findAllCellByColor(this, this.getAttribute("data-color"));
+	}
+	document.body.appendChild(canvas2);
+	
+	fill_global_arr_objects();
+	var result = findImageDataInArrObjects( global_arr_objects, imgData.data );
+	if(result!=null)
+	{
+		var color = getRndColor();
+		canvas2.setAttribute("data-color", color);
+		//alert(global_arr_objects[result].points);	
+		for(var i=0;i<global_arr_objects[result].points.length;i++)
+		{
+			var p = global_arr_objects[result].points[i];
+			context.fillStyle = color;
+			context.fillRect(p[0],p[1],global_seed_size,global_seed_size);
+			
+			
+		}
+	}
+	
+	e.preventDefault();
+	
+	*****/
+	}
+}
+
+function exchange(e)
+{
+	if(global_first_seed_image_data != null)	
+	{
+		//alert('test exch ok');
+		
+		var second_selected_seed = get_selected_seed(e);
+		if(second_selected_seed == null) return;	
+	
+		var canvas = document.getElementById("left_canvas");
+		var context = canvas.getContext("2d");
+		//alert('test exch ok #2');
+		var n2 = second_selected_seed[2];
+		var m2 = second_selected_seed[3];
+		
+		var n = global_selected_seed[2];
+		var m = global_selected_seed[3];
+		
+		if(n==n2 && m==m2) 
+		{
+			context.putImageData(global_first_seed_image_data,n2*global_seed_size,m2*global_seed_size);
+			//alert('test == ok');
+			return;
+		}
+		
+		var tImgData1 = global_first_seed_image_data;//context.getImageData(n2*global_seed_size,m2*global_seed_size,global_seed_size,global_seed_size);
+		
+		var imgData2 = context.getImageData(n2*global_seed_size,m2*global_seed_size,global_seed_size,global_seed_size);
+		
+		var tImgData2 = cloneUnrealImageData(n2,m2,imgData2);
+		///var tImgData1 = cloneUnrealImageData(n,m,imgData);
+		
+		//510 red
+		context.putImageData(tImgData1,n2*global_seed_size,m2*global_seed_size);  //2 <= 1   1<=tim   2?
+		
+		context.putImageData(tImgData2,n*global_seed_size,m*global_seed_size);
+		
+		//context.putImageData(tImgData,n2*global_seed_size,m2*global_seed_size);
+		
+		global_first_seed_image_data = null;
+		second_selected_seed = null;
+		global_selected_seed == null;
+		//var imgData2 = cloneImageData(imgData);
+		
+		//context.fillStyle = "rgba(0,255,255,0.5)";
+		//context.fillRect(n*global_seed_size,m*global_seed_size,global_seed_size,global_seed_size);
+		
+	}	
+	
+}
+
+function cloneUnrealImageData(n,m,imgData)
+{
+	/********
+	var second_selected_seed = get_selected_seed(e);
+	if(second_selected_seed == null) return;	
+	var n = second_selected_seed[2];
+	var m = second_selected_seed[3];
+	*******/
+	
+	var canvas = document.getElementById("left_canvas");
+	var context = canvas.getContext("2d");
+	var imgData0 = context.getImageData(n*global_seed_size,m*global_seed_size,global_seed_size,global_seed_size);
+	
+	var canvas2 = document.createElement("canvas");
+	canvas2.width = global_seed_size;
+	canvas2.height = global_seed_size;
+	var ctx = canvas2.getContext("2d");
+	ctx.putImageData(imgData0, 0,0);
+	
+	return ctx.getImageData(0,0,global_seed_size,global_seed_size);
+	
+		
+}	
+
+function matrixmove()
+{
+	
+}	
+
 function whenUserLeftClickOnLeftCanvas(e)
 {
 	//alert('left');
 	if(global_selected_seed == null)	
 	{
 		global_selected_seed = get_selected_seed(e);
-		put_one_seed_into_right_canvas();
+		highlight();
+		//put_one_seed_into_right_canvas();
 	}
 	else 
 	{
 		//put_one_seed_into_right_canvas(); //zmey za hvost sebya kusaet 
 		
 		var second_seed = get_selected_seed(e);
-		fill_right_canvas( second_seed );
+		exchange(e);
+		//fill_right_canvas( second_seed );
 		global_selected_seed = null;
 	}
+	/*******/
+	
+	matrixmove();
 	
 }
 
@@ -434,6 +594,23 @@ function whenUserLeftClickOnRightCanvas()
 		clear_right_canvas();
 		global_selected_seed = null;
 	}
+	
+	if(second_selected_seed!=null && global_selected_seed!=null)
+	{
+	var n2 = second_selected_seed[2];
+		var m2 = second_selected_seed[3];
+		
+		var n = global_selected_seed[2];
+		var m = global_selected_seed[3];
+		
+		if(n==n2 && m==m2) 
+		{
+			//context.putImageData(global_first_seed_image_data,n2*global_seed_size,m2*global_seed_size);
+			
+			return;
+		}
+	}
+	matrixmove();
 }
 
 

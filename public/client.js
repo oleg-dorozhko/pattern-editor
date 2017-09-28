196690b246a21b6148b2971a1f312ebc34602628
oleg-dorozhko <oleg.dorozhko@gmail.com>
2017-09-28 18:17:13 +0300

    new very cool update 2

--- public/client.js
+++ public/client.js
@@ -374,9 +374,11 @@ window.onload = function()
 		***/
 		
 	});
+	document.getElementById('close_attention').onclick = function(){
+		fadeOut( document.getElementById('attention'), 100 );
+	}
 	
-	
-	
+	 fadeOut( document.getElementById('attention'), 20*1000 );
 	
 }
 
@@ -393,6 +395,72 @@ function clearSelection() {
     }
   }
 
+  
+  function fadeIn( elem, ms )
+{
+  if( ! elem )
+    return;
+
+  elem.style.opacity = 0;
+  elem.style.filter = "alpha(opacity=0)";
+  elem.style.display = "inline-block";
+  elem.style.visibility = "visible";
+
+  if( ms )
+  {
+    var opacity = 0;
+    var timer = setInterval( function() {
+      opacity += 50 / ms;
+      if( opacity >= 1 )
+      {
+        clearInterval(timer);
+        opacity = 1;
+      }
+      elem.style.opacity = opacity;
+      elem.style.filter = "alpha(opacity=" + opacity * 100 + ")";
+    }, 50 );
+  }
+  else
+  {
+    elem.style.opacity = 1;
+    elem.style.filter = "alpha(opacity=1)";
+  }
+}
+
+function fadeOut( elem, ms )
+{
+  if( ! elem )
+    return;
+
+  if( ms )
+  {
+    var opacity = 1;
+    var timer = setInterval( function() {
+      opacity -= 50 / ms;
+      if( opacity <= 0 )
+      {
+        clearInterval(timer);
+        opacity = 0;
+        elem.style.display = "none";
+        elem.style.visibility = "hidden";
+      }
+      elem.style.opacity = opacity;
+      elem.style.filter = "alpha(opacity=" + opacity * 100 + ")";
+    }, 50 );
+  }
+  else
+  {
+    elem.style.opacity = 0;
+    elem.style.filter = "alpha(opacity=0)";
+    elem.style.display = "none";
+    elem.style.visibility = "hidden";
+  }
+}
+
+function handleClick( evt )
+{
+  fadeOut( document.getElementById(evt.target.id), 400 );
+}
 
 
 window.onerror = function(message, url, lineNumber) {
